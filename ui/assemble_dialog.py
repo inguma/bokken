428af053cd2a6b5002cde9c9f9bbdfcf1fb3f77b
Hugo Teso <hugo.teso@gmail.com>
2011-10-03 18:35:06 +0200

    Improved assembler, now works :)

--- ui/assemble_dialog.py
+++ ui/assemble_dialog.py
@@ -17,13 +17,15 @@
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 
+import os
 import gtk
+import gtksourceview2
 
 class AssembleDialog(gtk.Dialog):
-    '''Window to popup search output'''
+    '''Assembler plugin dialog'''
 
     def __init__(self, core):
-        super(AssembleDialog,self).__init__('Search results', None, gtk.DIALOG_DESTROY_WITH_PARENT, (gtk.STOCK_OK,gtk.RESPONSE_ACCEPT))
+        super(AssembleDialog,self).__init__('Assembler plugin', None, gtk.DIALOG_DESTROY_WITH_PARENT, (gtk.STOCK_OK,gtk.RESPONSE_ACCEPT))
 
         self.uicore = core
 
@@ -40,37 +42,61 @@ class AssembleDialog(gtk.Dialog):
 
         # Log TextView
         #################################################################
-        self.output_text = gtk.TextView(buffer=None)
-        self.output_text.set_wrap_mode(gtk.WRAP_NONE)
-        self.output_text.set_editable(True)
-        self.output_buffer = self.output_text.get_buffer()
-        self.output_buffer.set_text('Not yet working!')
+        # Add ui dir to language paths
+        lm = gtksourceview2.LanguageManager()
+        paths = lm.get_search_path()
+        paths.append(os.getcwd() + os.sep + 'ui' + os.sep + 'data' + os.sep)
+        lm.set_search_path(paths)
+
+        self.output_buffer = gtksourceview2.Buffer()
+        self.output_view = gtksourceview2.View(self.output_buffer)
+        self.output_view.set_show_right_margin(True)
+        self.output_view.set_right_margin(10)
+        self.output_view.set_editable(False)
+
+        self.output_buffer.set_data('languages-manager', lm)
+        self.output_buffer.set_highlight_syntax(True)
+
+        manager = self.output_buffer.get_data('languages-manager')
+        language = manager.get_language('asm')
+        self.output_buffer.set_language(language)
 
         self.scrolled_window = gtk.ScrolledWindow()
         self.scrolled_window.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_ALWAYS)
         self.scrolled_window.is_visible = True
 
         # Add Textview to Scrolled Window
-        self.scrolled_window.add_with_viewport(self.output_text)
+        self.scrolled_window.add_with_viewport(self.output_view)
 
-        #self.vbox.pack_start(self.output_text)
         self.vbox.pack_start(self.scrolled_window)
 
         # Bytes text entry
         self.bytes_hb = gtk.HBox(False, False)
         self.bytes_entry = gtk.Entry(100)
+        self.bytes_entry.set_tooltip_text('Ex: 9090e900000000')
+        self.bytes_entry.connect('activate', self.disasm)
+
         self.bytes_hb.add(self.bytes_entry)
-        self.bytes_button = gtk.Button(label="disassemble")
+        self.bytes_button = gtk.Button(label="Disassemble")
         self.bytes_button.connect('clicked', self.disasm)
+        w,h = self.bytes_button.size_request()
+        self.bytes_button.set_size_request(85, h)
+
         self.bytes_hb.pack_start(self.bytes_button, False, False)
         self.vbox.pack_start(self.bytes_hb, False, False)
 
         # Offset text entry
         self.offset_hb = gtk.HBox(False, False)
         self.offset_entry = gtk.Entry(100)
+        self.offset_entry.set_tooltip_text('Ex: nop; nop; jmp 0x10')
+        self.offset_entry.connect('activate', self.assemble)
+
         self.offset_hb.add(self.offset_entry)
-        self.offset_button = gtk.Button(label="write")
+        self.offset_button = gtk.Button(label="Assemble")
         self.offset_button.connect('clicked', self.assemble)
+        w,h = self.offset_button.size_request()
+        self.offset_button.set_size_request(85, h)
+
         self.offset_hb.pack_start(self.offset_button, False, False)
         self.vbox.pack_start(self.offset_hb, False, False)
 
@@ -79,19 +105,20 @@ class AssembleDialog(gtk.Dialog):
     #
     # Methods
 
-    def assemble(self, widget, opcode):
-        a = self.uicore.assembler
-        a.set_pc(int(self.offset_entry.get_text()))
-        code = a.massemble(opcode)
+    def assemble(self, widget):
+        offset = self.offset_entry.get_text()
+        #a.set_pc( int(offset) )
+        code = self.uicore.core.assembler.massemble(offset)
         if code:
-            print "HEX: %s" % code.buf_hex
+            self.output_buffer.set_text( ";; HEX:\n%s" % code.buf_hex )
         # TODO self.core.cmd0(wx HEX @ ADDR)
         # TODO refresh disasm/hex buffers :(
 
-    def disasm(self, widget, bytes):
-        a = self.uicore.assembler
-        a.set_pc(int(self.offset_entry.get_text()))
-        code = a.mdisassemnble_hexstr(bytes)
+    def disasm(self, widget):
+        bytes = self.bytes_entry.get_text()
+        #a.set_pc( int(bytes) )
+        code = self.uicore.core.assembler.mdisassemble_hexstr(bytes)
         if code:
-            print "ASM: %s" % code.buf_asm
+            #print "ASM: %s" % code.buf_asm
+            self.output_buffer.set_text( ";; ASM:\n%s" % code.buf_asm )
         # TODO: show popup with disasm stuff
