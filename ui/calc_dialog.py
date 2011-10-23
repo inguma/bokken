90228224f8b4383de253ba46653636e895fa310b
Hugo Teso <hugo.teso@gmail.com>
2011-10-23 20:43:04 +0200

    Some calculator improvements. Closes #80

--- ui/calc_dialog.py
+++ ui/calc_dialog.py
@@ -27,6 +27,11 @@ class CalcDialog(gtk.Dialog):
 
         self.uicore = core
 
+        # Set dialog resizeable and auto-shrink
+        self.set_policy(False, False, True)
+
+        self.examples = "Examples:\n0x100\n0x100 + 20\n01001\n01001 + 0x10\n0x10 + 20 * 0101"
+
         # the cancel button
         self.butt_ok = self.action_area.get_children()[0]
         self.butt_ok.connect("clicked", lambda x: self.destroy())
@@ -45,7 +50,7 @@ class CalcDialog(gtk.Dialog):
         # TextEntry
         self.input_entry = gtk.Entry(100)
         self.input_entry.set_icon_from_stock(1, gtk.STOCK_INFO)
-        self.input_entry.set_icon_tooltip_text(1, 'Enter expression')
+        self.input_entry.set_icon_tooltip_text(1, self.examples)
         self.input_entry.connect("activate", self._do_calc)
         self.input_entry.connect("icon-press", self._do_calc)
         #self.input_entry.set_sensitive(False)
@@ -60,14 +65,49 @@ class CalcDialog(gtk.Dialog):
         # Pack elements into hbox
         self.hbox2.pack_start(self.input_entry2, True, True, 2)
 
+        # Base conversion labels
+        self.out_box = gtk.VBox(False, 1)
+        self.hex_lbl = gtk.Label()
+        self.hex_lbl.set_alignment(0, 0.5)
+        self.dec_lbl = gtk.Label()
+        self.dec_lbl.set_alignment(0, 0.5)
+        self.oct_lbl = gtk.Label()
+        self.oct_lbl.set_alignment(0, 0.5)
+        self.bin_lbl = gtk.Label()
+        self.bin_lbl.set_alignment(0, 0.5)
+        self.chr_lbl = gtk.Label()
+        self.chr_lbl.set_alignment(0, 0.5)
+
+        self.out_box.pack_start(self.hex_lbl, False, False, 1)
+        self.out_box.pack_start(self.dec_lbl, False, False, 1)
+        self.out_box.pack_start(self.oct_lbl, False, False, 1)
+        self.out_box.pack_start(self.bin_lbl, False, False, 1)
+        self.out_box.pack_start(self.chr_lbl, False, False, 1)
+
         # Pack elements into main_vbox
         self.main_vbox.pack_start(self.label, False, False, 2)
         self.main_vbox.pack_start(self.hbox1, False, False, 2)
         self.main_vbox.pack_start(self.hbox2, False, False, 2)
+        self.main_vbox.pack_start(self.out_box, False, False, 2)
 
         self.vbox.pack_start(self.main_vbox)
         self.show_all()
+        self.out_box.hide_all()
 
     def _do_calc(self, widget, icon_pos=None, event=None):
         res = self.uicore.core.num.math(self.input_entry.get_text())
-        self.input_entry2.set_text(str(res))
+        if res:
+            self.input_entry2.set_text(str(res))
+    
+            self.hex_lbl.set_markup( "<b>Hexacecimal:</b>\t" + hex(res) )
+            self.dec_lbl.set_markup( "<b>Decimal</b>:\t\t" + str(int(str(res), 10)) )
+            self.oct_lbl.set_markup( "<b>Octal:</b>\t\t" + str(int(str(res), 8)) )
+            self.bin_lbl.set_markup( "<b>Binary:</b>\t\t" + bin(res)[2:] )
+            try:
+                self.chr_lbl.set_markup( "<b>Character:</b>\t" + chr(res))
+            except:
+                self.chr_lbl.set_markup( "<b>Character:</b>\t")
+            self.out_box.show_all()
+        else:
+            self.input_entry2.set_text('')
+            self.out_box.hide_all()
