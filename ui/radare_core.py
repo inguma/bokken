71837bffea2a18e74e7989dfadc0825ea33b5634
David Martínez Moreno <ender@debian.org>
2014-06-16 18:05:03 -0700

    Added useful intermediate debug functions to radare core to be able to intercept traffic to and from the core.

--- ui/radare_core.py
+++ ui/radare_core.py
@@ -1,19 +1,17 @@
-#!/usr/bin/python
-
 #       radare_core.py
-#       
+#
 #       Copyright 2011 Hugo Teso <hugo.teso@gmail.com>
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 2 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -33,6 +31,7 @@ class Core():
         self.cons = RCons()
         self.assembler = self.core.assembler
         self.core.format = ''
+        self.debug = False
 
         self.set_options(dialog)
 
@@ -68,6 +67,26 @@ class Core():
 
         self.backend = 'radare'
         self.file_loaded = False
+
+    def send_cmd(self, string):
+        '''This function is in the middle of the communication of the core and
+        Bokken to be able to intercept input and output.'''
+        if self.debug:
+            print('DEBUG IN : %s' % string)
+        output = self.core.cmd0(string)
+        if self.debug:
+            print('DEBUG OUT: %s' % output)
+        return output
+
+    def send_cmd_str(self, string):
+        '''This function is in the middle of the communication of the core and
+        Bokken to be able to intercept input and output.'''
+        if self.debug:
+            print('DEBUG IN : %s' % string)
+        output = self.core.cmd_str(string)
+        if self.debug:
+            print('DEBUG OUT: %s' % output)
+        return output
 
     def clean_fullvars(self):
         self.fulldasm = ''
@@ -125,27 +144,27 @@ class Core():
         self.core.bin_load(None, 0)
         #print "[*] Set config options"
         #self.core.config.set("asm.bytes", "false")
-        self.core.cmd0("e scr.interactive=false")
-        self.core.cmd0('e asm.lines=false')
-        self.core.cmd0('e scr.color=0')
+        self.send_cmd("e scr.interactive=false")
+        self.send_cmd('e asm.lines=false')
+        self.send_cmd('e scr.color=0')
         if not self.lower_case:
-            self.core.cmd0('e asm.ucase=true')
+            self.send_cmd('e asm.ucase=true')
         else:
-            self.core.cmd0('e asm.ucase=false')
+            self.send_cmd('e asm.ucase=false')
         if self.use_va:
-            self.core.cmd0("e io.va=0")
+            self.send_cmd("e io.va=0")
         else:
-            self.core.cmd0("e io.va=1")
+            self.send_cmd("e io.va=1")
         if self.asm_syntax:
-            self.core.cmd0("e asm.syntax=att")
+            self.send_cmd("e asm.syntax=att")
         else:
-            self.core.cmd0("e asm.syntax=intel")
+            self.send_cmd("e asm.syntax=intel")
         if self.asm_bytes:
-            self.core.cmd0("e asm.bytes=false")
+            self.send_cmd("e asm.bytes=false")
         else:
-            self.core.cmd0("e asm.bytes=true")
+            self.send_cmd("e asm.bytes=true")
         if self.do_anal:
-            self.core.cmd0("aa")
+            self.send_cmd("aa")
 
         self.bin = self.core.bin
         #print self.bin
@@ -154,7 +173,7 @@ class Core():
         #print "[*] Got bin info"
         if not self.info:
             #print "[*] No bin info"
-            list = self.core.cmd_str('i').split('\n')
+            list = self.send_cmd_str('i').split('\n')
             for line in list:
                 if line:
                     line = line.split('\t')
@@ -166,7 +185,7 @@ class Core():
         #print "[*] Get base addr"
         self.baddr = self.bin.get_baddr()
         self.size = hex(self.core.file.size)
-        self.core.cmd0('e search.flags=false')
+        self.send_cmd('e search.flags=false')
         if self.do_anal:
             self.core.format = 'Program'
         else:
@@ -193,9 +212,9 @@ class Core():
 
     def restore_va(self):
         if self.use_va:
-            self.core.cmd0("e io.va=0")
+            self.send_cmd("e io.va=0")
         else:
-            self.core.cmd0("e io.va=1")
+            self.send_cmd("e io.va=1")
 
     def is_url(self, file):
         #print "[*] Checking if is URL"
@@ -210,12 +229,12 @@ class Core():
             #print "[*] Get strings"
             self.update_progress_bar("Getting strings", 0.6)
             if self.use_va:
-                self.core.cmd0('e io.va=0')
+                self.send_cmd('e io.va=0')
             else:
-                self.core.cmd0('e io.va=1')
+                self.send_cmd('e io.va=1')
             strings = ''
-            self.core.cmd0('fs strings')
-            strings = self.core.cmd_str('f')
+            self.send_cmd('fs strings')
+            strings = self.send_cmd_str('f')
             self.allstrings = strings
         return self.allstrings
 
@@ -223,8 +242,8 @@ class Core():
         if not self.allfuncs:
             #print "[*] Get functions"
             self.update_progress_bar("Getting functions", 0.8)
-            #self.core.cmd0('aa')
-            #self.core.cmd0('fs functions')
+            #self.send_cmd('aa')
+            #self.send_cmd('fs functions')
             #if self.bin.get_sym(0):
             #    self.allfuncs.append('entry0')
             if self.bin.get_sym(1):
@@ -233,7 +252,7 @@ class Core():
                 self.allfuncs.append('main')
             if self.bin.get_sym(3):
                 self.allfuncs.append('sym._fini')
-            for fcn in self.core.cmd_str('afl').split('\n'):
+            for fcn in self.send_cmd_str('afl').split('\n'):
                 if fcn:
                     #print ' 0x%08x' % fcn.addr, fcn.name
                     fcn = fcn.split(' ')[-1]
@@ -243,29 +262,29 @@ class Core():
     def get_hexdump(self):
         #print "[*] Get hexdump"
         self.update_progress_bar("Getting hexdump", 0.75)
-        #self.core.cmd0('e io.va=0')
-        hexdump = self.core.cmd_str('px')
-        #self.core.cmd0('e io.va=1')
+        #self.send_cmd('e io.va=0')
+        hexdump = self.send_cmd_str('px')
+        #self.send_cmd('e io.va=1')
         return hexdump
 
     def get_full_hexdump(self):
         if self.fullhex == '' and self.size != '0x0':
             #print "[*] Get full hexdump"
             self.update_progress_bar("Getting full hexdump", 0.5)
-            self.core.cmd0('e io.va=0')
-            self.core.cmd0('s 0')
-            self.core.cmd0('b ' + str(self.size))
-            hexdump = self.core.cmd_str('px')
-            self.core.cmd0('s ' + str(self.baddr))
-            self.core.cmd0('b 512')
-            #self.core.cmd0('e io.va=1')
+            self.send_cmd('e io.va=0')
+            self.send_cmd('s 0')
+            self.send_cmd('b ' + str(self.size))
+            hexdump = self.send_cmd_str('px')
+            self.send_cmd('s ' + str(self.baddr))
+            self.send_cmd('b 512')
+            #self.send_cmd('e io.va=1')
             self.fullhex = hexdump
         return self.fullhex
 
     def get_dasm(self):
         #print "[*] Get dasm"
         self.update_progress_bar("Getting dasm",0.25)
-        dasm = self.core.cmd_str('pd')
+        dasm = self.send_cmd_str('pd')
         return dasm
 
     def get_text_dasm(self):
@@ -279,13 +298,13 @@ class Core():
             for section in self.execsections:
                 percent += step
                 dasm = ''
-                self.core.cmd0('s section.' + section[0])
-                self.core.cmd0('b ' + str(section[1]))
+                self.send_cmd('s section.' + section[0])
+                self.send_cmd('b ' + str(section[1]))
                 print "\t* Let's get the dasm for %s..." % section[0],
                 #self.update_progress_bar("Reading assembler for section %s..." % section[0], percent)
-                dasm = self.core.cmd_str('pD')
+                dasm = self.send_cmd_str('pD')
                 self.sections_lines.append( len(dasm.split('\n')) )
-                self.core.cmd0('b 512')
+                self.send_cmd('b 512')
                 print " OK!"
                 self.text_dasm += dasm
                 if percent == base_percent + step * 10:
@@ -300,10 +319,10 @@ class Core():
     def get_fulldasm(self):
         if not self.fulldasm:
             #print "[*] Get full dasm"
-            self.core.cmd0('s ' + str(self.baddr))
-            self.core.cmd0('b ' + str(self.size))
-            dasm = self.core.cmd_str('pd')
-            self.core.cmd0('b 512')
+            self.send_cmd('s ' + str(self.baddr))
+            self.send_cmd('b ' + str(self.size))
+            dasm = self.send_cmd_str('pd')
+            self.send_cmd('b 512')
             self.fulldasm = dasm
         return self.fulldasm
 
@@ -319,14 +338,14 @@ class Core():
         if not self.fullstr:
             #print "[*] Get string repr"
             self.update_progress_bar("Getting string representation", 0.65)
-            self.core.cmd0('e io.va=0')
-            self.core.cmd0('s 0')
-            self.core.cmd0('b ' + str(self.size))
-            self.fullstr = self.core.cmd_str('ps ' + str(self.size))
-            #self.core.cmd0('b 1024')
-            #self.core.cmd0('e io.va=1')
-            self.core.cmd0('s section..text')
-            self.core.cmd0('b 512')
+            self.send_cmd('e io.va=0')
+            self.send_cmd('s 0')
+            self.send_cmd('b ' + str(self.size))
+            self.fullstr = self.send_cmd_str('ps ' + str(self.size))
+            #self.send_cmd('b 1024')
+            #self.send_cmd('e io.va=1')
+            self.send_cmd('s section..text')
+            self.send_cmd('b 512')
         return self.fullstr
 
     def get_sections(self):
@@ -400,16 +419,16 @@ class Core():
             sct = False
             for section in self.execsections:
                 if '.text' in section:
-                    self.core.cmd0(cmd + 'section..text > ' + file.name)
+                    self.send_cmd(cmd + 'section..text > ' + file.name)
                     sct = False
                     break
                 else:
                     sct = True
             if sct:
-                self.core.cmd0(cmd + 'section' + self.execsections[0][0] +' > ' + file.name)
-            #self.core.cmd_str('aga > ' + file.name)
+                self.send_cmd(cmd + 'section' + self.execsections[0][0] +' > ' + file.name)
+            #self.send_cmd_str('aga > ' + file.name)
         else:
-            self.core.cmd0(cmd + addr + ' > ' + file.name)
+            self.send_cmd(cmd + addr + ' > ' + file.name)
         file.close()
         f = open(file.name, 'r')
         self.dot = f.read()
@@ -433,42 +452,42 @@ class Core():
         self.update_progress_bar("Loading full file structure", 0.2)
         if not self.full_fileinfo:
             # get binary info
-            file_info = self.core.cmd_str('iI')
+            file_info = self.send_cmd_str('iI')
             if file_info:
                 self.full_fileinfo['bin'] = []
                 for line in file_info.split('\n'):
                     line = line.split('=')
                     self.full_fileinfo['bin'].append(line)
             # Get imports
-            imports = self.core.cmd_str('ii')
+            imports = self.send_cmd_str('ii')
             if imports:
                 self.full_fileinfo['imports'] = []
                 for line in imports.split('\n'):
                     line = line.split(' ')
                     self.full_fileinfo['imports'].append(line)
             # Get entry points
-            entryp = self.core.cmd_str('ie')
+            entryp = self.send_cmd_str('ie')
             if entryp:
                 self.full_fileinfo['eps'] = []
                 for line in entryp.split('\n'):
                     line = line.split(' ')
                     self.full_fileinfo['eps'].append(line)
             # Get symbols
-            symbols = self.core.cmd_str('is')
+            symbols = self.send_cmd_str('is')
             if symbols:
                 self.full_fileinfo['symbols'] = []
                 for line in symbols.split('\n'):
                     line = line.split(' ')
                     self.full_fileinfo['symbols'].append(line)
             # Get sections
-            sections = self.core.cmd_str('iS')
+            sections = self.send_cmd_str('iS')
             if sections:
                 self.full_fileinfo['sections'] = []
                 for line in sections.split('\n'):
                     line = line.split(' ')
                     self.full_fileinfo['sections'].append(line)
             # Get strings
-            strings = self.core.cmd_str('iz')
+            strings = self.send_cmd_str('iz')
             if strings:
                 self.full_fileinfo['strings'] = []
                 for line in strings.split('\n'):
@@ -476,26 +495,26 @@ class Core():
                     self.full_fileinfo['strings'].append(line)
 
     def get_magic(self):
-        self.core.cmd0('e io.va=0')
-        self.ars_magica = self.core.cmd_str('pm')
-        #self.core.cmd0('e io.va=true')
+        self.send_cmd('e io.va=0')
+        self.ars_magica = self.send_cmd_str('pm')
+        #self.send_cmd('e io.va=true')
         #print self.ars_magica
 
     def seek(self, pos):
         #print pos
-        self.core.cmd0('s ' + str(pos))
+        self.send_cmd('s ' + str(pos))
         return True
 
     def set_bsize(self, size):
-        self.core.cmd0('b ' + str(size))
+        self.send_cmd('b ' + str(size))
         return True
 
     def execute_command(self, command):
-        res = self.core.cmd_str(command)
+        res = self.send_cmd_str(command)
         return res
 
     def add_comment(self, offset, comment):
-        self.core.cmd0('CC 1 ' + comment + ' @ ' + offset)
+        self.send_cmd('CC 1 ' + comment + ' @ ' + offset)
         return True
 
     def move(self, direction, output):
@@ -504,29 +523,29 @@ class Core():
         elif direction == 'b':
             direction = '.'
 
-        va = self.core.cmd_str('e io.va').rstrip()
+        va = self.send_cmd_str('e io.va').rstrip()
 
         if output == 'hexadecimal':
             if va == "true":
-                self.core.cmd0('e io.va=0')
+                self.send_cmd('e io.va=0')
             self.core._cmd('px', True)
-            data = self.core.cmd_str(direction)
-            #self.core.cmd0('e io.va=true')
+            data = self.send_cmd_str(direction)
+            #self.send_cmd('e io.va=true')
         elif output == 'disassembly':
             if va == "false":
-                self.core.cmd0('e io.va=1')
+                self.send_cmd('e io.va=1')
             self.core._cmd('pd', True)
-            data = self.core.cmd_str(direction)
+            data = self.send_cmd_str(direction)
 
         return data
 
     def string_search(self, text, type):
         '''Searches an arbitrary string text in the analyzed content.'''
-        self.core.cmd0('e io.va=0')
-        self.core.cmd0('s 0')
+        self.send_cmd('e io.va=0')
+        self.send_cmd('s 0')
         results = {}
 
-        hits = self.core.cmd_str('/' + type + text).rstrip().split('\n')
+        hits = self.send_cmd_str('/' + type + text).rstrip().split('\n')
         # The first line is 'fs hits', so discard it.
         hits = hits[1:]
         # The output is:
@@ -539,7 +558,7 @@ class Core():
         # for the time being we put an empty string as the second element.
         results = map(lambda x: (x.split(' ')[-1], ''), hits)
 
-        #self.core.cmd0('e io.va=1')
+        #self.send_cmd('e io.va=1')
         self.restore_va()
         return results
 
